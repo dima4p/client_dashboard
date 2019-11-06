bb018fc8d254f5d5549bfceb63b83cc2aee946d5
Dmitri Koulikoff <dima@koulikoff.ru>
2019-11-06 22:59:31 +0100

    allow json push and patch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: 'username', password: 'secret'
+  protect_from_forgery unless: -> { request.format.json? }
 end
